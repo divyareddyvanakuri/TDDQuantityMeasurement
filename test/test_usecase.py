a726dd876982a2bb95beb50e192342a96fb749a9
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-22 18:54:32 +0530

    [Divya]refactor: Implemented the test method for null check , given none and zero feet are compared then should not equal

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -7,3 +7,10 @@ def test_givenZeroFeetAndZeroFeet_whenCompared_thenShouldEqual():
     first_zeroFeet_object = FeetUnit(0) 
     second_zeroFeet_object = FeetUnit(0)
     assert first_zeroFeet_object == second_zeroFeet_object
+
+
+def test_givenNoneAndZeroFeet_whenCompared_thenShouldNotEqual():
+    with pytest.raises(AssertionError) as e:
+        none_feet_object = None
+        zero_feet_object = FeetUnit(0)
+        assert none_feet_object != zero_feet_object
