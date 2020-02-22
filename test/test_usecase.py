8c354ea997cb1edadb41fb29197777e8e90f4c1f
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-22 18:49:32 +0530

    [Divya]refactor: import the sys module and append the path for feetunit class the imported

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -1,3 +1,8 @@
+import pytest
+import sys
+sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
+from quantitymeasurment import FeetUnit
+
 def test_givenZeroFeetAndZeroFeet_whenCompared_thenShouldEqual():
     first_zeroFeet_object = FeetUnit(0) 
     second_zeroFeet_object = FeetUnit(0)
