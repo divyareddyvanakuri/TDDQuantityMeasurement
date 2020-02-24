3f54a03f408d587d91028d6fd848c93d66c3a263
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 16:50:12 +0530

    [Divya]refactor:Implemented the test method for given 1 tonne  and 1000 grams added then result should in kgs

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -13,6 +13,12 @@ def test_givenOneKgAndThousandGrams_whenCompared_thenShouldEqual():
 
 
 def test_givenOneTonneAndThousandKg_whenCompared_thenShouldEqual():
-    one_kg_object = Quantity(1,TonneUnit())
-    thousand_grams_object = Quantity(1000,KgUnit())
-    assert one_kg_object == thousand_grams_object
+    one_tonne_object = Quantity(1,TonneUnit())
+    thousand_kgs_object = Quantity(1000,KgUnit())
+    assert one_tonne_object == thousand_kgs_object
+
+
+def test_givenOneTonneAndThousandKg_whenAdded_thenResultShouldInKgs():
+    one_tonne_object = Quantity(1,TonneUnit())
+    thousand_grams_object = Quantity(1000,GramUnit())
+    assert one_tonne_object + thousand_grams_object == 1001
