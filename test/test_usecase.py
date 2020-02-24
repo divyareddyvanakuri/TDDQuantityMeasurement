1228e1ab8be561ca5975b876840d63670ee320fb
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 15:44:40 +0530

    [Divya]refactor:Implemented the test method for given 1 gallon and 3.78 litre comapred should equal

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -1,30 +1,9 @@
-import pytest
-import sys
-sys.path.append("/home/user/Desktop/TDDQuantityMeasurment")
-from main import Quantity
-sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
-from quantitymeasurment import InchUnit,FeetUnit,CentiMeterUnit
+def test_givenOneGallonAndThreePointSevenEightLitres_whenCompared_thenShouldEqual():
+    one_gallon_object = Quantity(1,GallonUnit())
+    three_point_seven_eight_litre_object = Quantity(3.78,LitreUnit())
+    assert one_gallon_object == three_point_seven_eight_litre_object
 
 
-def test_givenTwoInchAndTwoInch_whenAdded_thenResultShouldInInches():
-    first_two_inch_object = Quantity(2,InchUnit())
-    second_two_inch_object = Quantity(2,InchUnit())
-    assert first_two_inch_object + second_two_inch_object == 4
 
 
-def test_givenOneFeetAndTwoInch_whenAdded_thenResultShouldInInches():
-    one_feet_object = Quantity(1,FeetUnit())
-    two_inch_object = Quantity(2,InchUnit())
-    assert one_feet_object + two_inch_object == 14
 
-
-def test_givenOneFeetAndOneFeet_whenAdded_thenResultShouldInInches():
-    first_one_feet_object = Quantity(1,FeetUnit())
-    second_one_feet_object = Quantity(1,FeetUnit())
-    assert first_one_feet_object + second_one_feet_object == 24
-
-
-def test_givenTwoInchAndTwoPointFiveCentiMeter_whenAdded_thenResultShouldInInches():
-    two_inch_object = Quantity(2,InchUnit())
-    twoPointFive_cm_object = Quantity(2.5,CentiMeterUnit())
-    assert two_inch_object + twoPointFive_cm_object == 3
