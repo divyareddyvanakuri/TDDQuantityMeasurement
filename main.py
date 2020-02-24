785e0bbf420a9cb0f8aee3e3a5484c5ba95a0596
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 15:29:28 +0530

    [Divya]refactor:Implemented the __add__ method two add two object instance variables

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
-from quantitymeasurment import CentiMeterUnit,InchUnit,Unit
+from quantitymeasurment import InchUnit,FeetUnit
 
 #This is the composition class for feet and inch objects
 class Quantity:
@@ -23,3 +23,7 @@ class Quantity:
                 return True
         return False
 
+    def __add__(self, other):
+        self.factor = int(self.unit.comparisons_to_base(self.value))
+        other.factor = int(other.unit.comparisons_to_base(other.value))
+        return self.factor+other.factor
