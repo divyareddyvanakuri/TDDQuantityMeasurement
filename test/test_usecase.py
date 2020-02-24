6fe2a8851f624df6cf8a9dfd39250628b5ffa15e
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 10:49:21 +0530

    [Divya]refactor:Implemented the test method for comparision check given 1 feet and 1 yard should return not equal

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -9,3 +9,10 @@ def test_givenThreeFeetAndOneYard_whenCompared_thenShouldEqual():
     three_feet_object = Quantity(3,FeetUnit()) 
     one_yard_object = Quantity(1,YardUnit())
     assert three_feet_object == one_yard_object
+
+
+def test_givenOneFeetAndOneYard_whenCompared_thenShouldNotEqual():
+    with pytest.raises(AssertionError) as e:
+        one_feet_object = Quantity(1,FeetUnit()) 
+        one_yard_object = Quantity(1,YardUnit())
+        assert one_feet_object != one_yard_object
