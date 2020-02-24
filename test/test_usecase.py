c2210b340d2114c05bd56e5cadcd20934749c99f
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 10:00:10 +0530

    [Divya]refactor:Implemented the test method for comparison check given 0 feet and 0 inch should return equal

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -1,43 +1,52 @@
 import pytest
 import sys
+sys.path.append("/home/user/Desktop/TDDQuantityMeasurment")
+from main import Quantity
 sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
-from quantitymeasurment import FeetUnit,InchUnit
+from quantitymeasurment import InchUnit,FeetUnit
 
 def test_givenZeroFeetAndZeroFeet_whenCompared_thenShouldEqual():
-    first_zeroFeet_object = FeetUnit(0) 
-    second_zeroFeet_object = FeetUnit(0)
+    first_zeroFeet_object = Quantity(0,FeetUnit()) 
+    second_zeroFeet_object = Quantity(0,FeetUnit())
     assert first_zeroFeet_object == second_zeroFeet_object
 
 
 def test_givenNoneAndZeroFeet_whenCompared_thenShouldNotEqual():
     with pytest.raises(AssertionError) as e:
         none_feet_object = None
-        zero_feet_object = FeetUnit(0)
+        zero_feet_object = Quantity(0,FeetUnit())
         assert none_feet_object != zero_feet_object
 
 
 def test_givenStringFeetAndZeroFeet_whenCompared_thenShouldNotEqual():
     with pytest.raises(AssertionError) as e:
-        string_feet_object = FeetUnit("0")
-        zero_feet_object = FeetUnit(0)
+        string_feet_object = Quantity("0",FeetUnit())
+        zero_feet_object = Quantity(0,FeetUnit())
         assert string_feet_object != zero_feet_object
 
 
 def test_givenZeroInchAndZeroInch_whenCompared_thenShouldEqual():
-    first_zeroInch_object = InchUnit(0)
-    second_zeroInch_object = InchUnit(0)
+    first_zeroInch_object = Quantity(0,InchUnit())
+    second_zeroInch_object = Quantity(0,InchUnit())
     assert first_zeroInch_object == second_zeroInch_object
 
 
 def test_givenNoneAndZeroInch_whenCompared_thenShouldNotEqual():
     with pytest.raises(AssertionError) as e:
         none_inch_object = None
-        zero_inch_object = InchUnit(0)
+        zero_inch_object = Quantity(0,InchUnit())
         assert none_inch_object != zero_inch_object
 
 
 def test_givenStringInchAndZeroInch_whenCompared_thenShouldNotEqual():
     with pytest.raises(AssertionError) as e:
-        string_inch_object = InchUnit("677")
-        zero_inch_object = InchUnit(0)
+        string_inch_object = Quantity("0",InchUnit())
+        zero_inch_object = Quantity(0,InchUnit())
         assert string_inch_object != zero_inch_object
+
+        
+def test_givenZeroFeetAndZeroInch_whenCompared_thenShouldEqual():
+    zero_feet_object = Quantity(0,FeetUnit())
+    zero_inch_object = Quantity(0,InchUnit())
+    assert zero_feet_object == zero_inch_object
+
