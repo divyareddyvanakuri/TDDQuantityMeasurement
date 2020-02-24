b0398b909450bdfbc4b789c56775d96cff2ba291
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 16:21:26 +0530

    [Divya]refactor:implemented the __add__ method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
-from quantitymeasurment import GallonUnit,LitreUnit,MlUnit
+from quantitymeasurment import MlUnit,GallonUnit,LitreUnit
 
 #This is the composition class for feet and inch objects
 class Quantity:
@@ -9,8 +9,13 @@ class Quantity:
         self.unit = unit
 
     def __eq__(self, other):
-        self.factor = int(self.unit.comparisons_to_base(self.value))
-        other.factor = int(other.unit.comparisons_to_base(other.value))
+        self.factor = float(self.unit.comparisons_to_base(self.value))
+        other.factor = float(other.unit.comparisons_to_base(other.value))
         if self.factor == other.factor:
             return True
         return False
+
+    def __add__(self, other):
+        self.factor = float(self.unit.comparisons_to_base(self.value))
+        other.factor = float(other.unit.comparisons_to_base(other.value))
+        return self.factor+other.factor
