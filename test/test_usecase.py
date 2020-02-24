7415e5cfb59a87a2e9e26b54d0a3d59e0c2a979c
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 14:57:03 +0530

    [Divya]refactor: Implemented the test method for addition given one feet and 2 inch then result should in inch

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -1,6 +1,20 @@
+import pytest
+import sys
+sys.path.append("/home/user/Desktop/TDDQuantityMeasurment")
+from main import Quantity
+sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
+from quantitymeasurment import InchUnit,FeetUnit
+
+
 def test_givenTwoInchAndTwoInch_whenAdded_thenResultShouldInInches():
-    first_two_inch_object = Quantity(5,InchUnit())
-    second_two_inch_object = Quantity(5,InchUnit())
-    assert first_two_inch_object == second_two_inch_object
+    first_two_inch_object = Quantity(2,InchUnit())
+    second_two_inch_object = Quantity(2,InchUnit())
+    assert (first_two_inch_object + second_two_inch_object == 4)
+
+
+def test_givenOneFeetAndTwoInch_whenAdded_thenResultShouldInInches():
+    one_feet_object = Quantity(1,FeetUnit())
+    two_inch_object = Quantity(2,InchUnit())
+    assert (one_feet_object + two_inch_object == 14)
 
 
