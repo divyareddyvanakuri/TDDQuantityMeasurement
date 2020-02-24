7b968afe553ab08309271e9a0d578f4ecc720525
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 16:02:57 +0530

    [Divya]refactor:removed the __ne__ method

--- main.py
+++ main.py
@@ -14,12 +14,3 @@ class Quantity:
         if self.factor == other.factor:
             return True
         return False
-
-    def __ne__(self, other):
-        if isinstance(other,self.__class__) and type(other.value) == type(self.value):
-            self.factor = int(self.unit.comparisons_to_base(self.value))
-            other.factor = int(other.unit.comparisons_to_base(other.value))
-            if self.factor == other.factor:
-                return True
-        return False
-
