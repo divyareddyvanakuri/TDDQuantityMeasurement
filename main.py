18faa7e28a85d04ce5b2219460ed04d8c0f07055
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 15:51:43 +0530

    [Divya]refactor:Implemented the test method for given 1 litre and 1000 ml comapred should equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
-from quantitymeasurment import InchUnit,FeetUnit
+from quantitymeasurment import GallonUnit,LitreUnit
 
 #This is the composition class for feet and inch objects
 class Quantity:
@@ -23,7 +23,3 @@ class Quantity:
                 return True
         return False
 
-    def __add__(self, other):
-        self.factor = int(self.unit.comparisons_to_base(self.value))
-        other.factor = int(other.unit.comparisons_to_base(other.value))
-        return self.factor+other.factor
