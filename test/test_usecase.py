fbbdcf2c1d85c631d50af26629575a887ed7e53a
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 16:34:53 +0530

    [Divya]refactor:Implemented the test method for given 1 kg and 1000 grams compared then result should equal

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -1,20 +1,5 @@
-import pytest
-import sys
-sys.path.append("/home/user/Desktop/TDDQuantityMeasurment")
-from main import Quantity
-sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
-from quantitymeasurment import GallonUnit,LitreUnit,MlUnit
-
-
-def test_givenOneGallonAndThreePointSevenEightLitres_whenAdded_thenResultShouldInLitres():
-    one_gallon_object = Quantity(1,GallonUnit())
-    three_point_seven_eight_litre_object = Quantity(3.78,LitreUnit())
-    assert one_gallon_object + three_point_seven_eight_litre_object == 7.56
-
-
-def test_givenOneLitreAndThousandMl_whenAdded_thenResultShouldInLitres():
-    one_litre_object = Quantity(1,LitreUnit())
-    thousand_ml_object = Quantity(1000,MlUnit())
-    assert one_litre_object + thousand_ml_object == 2.00
-
+def givenOneKgAndThousandGrams_whenCompared_thenShouldEqual():
+    one_kg_object = Quantity(1,KgUnit())
+    thousand_grams_object = Quantity(1000,GramsUnit())
+    assert one_kg_object == thousand_grams_object
 
