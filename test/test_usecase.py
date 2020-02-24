c9f92c893ceb61853895f9cfdcbbdbf132de7dfd
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 16:43:32 +0530

    [Divya]refactor:Implemented the test method for given 1 tonne  and 1000 kgs compared then result should equal

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -1,5 +1,18 @@
-def givenOneKgAndThousandGrams_whenCompared_thenShouldEqual():
+import pytest
+import sys
+sys.path.append("/home/user/Desktop/TDDQuantityMeasurment")
+from main import Quantity
+sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
+from quantitymeasurment import KgUnit,GramUnit,TonneUnit
+
+
+def test_givenOneKgAndThousandGrams_whenCompared_thenShouldEqual():
     one_kg_object = Quantity(1,KgUnit())
-    thousand_grams_object = Quantity(1000,GramsUnit())
+    thousand_grams_object = Quantity(1000,GramUnit())
     assert one_kg_object == thousand_grams_object
 
+
+def test_givenOneTonneAndThousandKg_whenCompared_thenShouldEqual():
+    one_kg_object = Quantity(1,TonneUnit())
+    thousand_grams_object = Quantity(1000,KgUnit())
+    assert one_kg_object == thousand_grams_object
