368f67866ff733a7ee4ab44003601da27c334c7b
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 16:12:55 +0530

    [Divya]refactor:Implemented the test method for given 1 gallon and 3.78 litre added then result should in litres

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -1,20 +1,9 @@
-import pytest
-import sys
-sys.path.append("/home/user/Desktop/TDDQuantityMeasurment")
-from main import Quantity
-sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
-from quantitymeasurment import GallonUnit,LitreUnit,MlUnit
-
-
-def test_givenOneGallonAndThreePointSevenEightLitres_whenCompared_thenShouldEqual():
+def test_givenOneGallonAndThreePointSevenEightLitres_whenAdded_thenResultShouldInLitres():
     one_gallon_object = Quantity(1,GallonUnit())
     three_point_seven_eight_litre_object = Quantity(3.78,LitreUnit())
-    assert one_gallon_object == three_point_seven_eight_litre_object
+    assert one_gallon_object+three_point_seven_eight_litre_object == 7.57
 
 
-def test_givenOneLitreAndThousandMl_whenCompared_thenShouldEqual():
-    one_litre_object = Quantity(1,LitreUnit())
-    thousand_ml_object = Quantity(1000,MlUnit())
-    assert one_litre_object  == thousand_ml_object
+
 
 
