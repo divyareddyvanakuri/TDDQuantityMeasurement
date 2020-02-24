5a7b7fc23a408aadcf6c9bf5ba8b4caaf6c63959
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 11:38:49 +0530

    [Divya]refactor: Implemented the test method compare give two inch and five cm then should return equal

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -1,43 +1,6 @@
-import pytest
-import sys
-sys.path.append("/home/user/Desktop/TDDQuantityMeasurment")
-from main import Quantity
-sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
-from quantitymeasurment import FeetUnit,YardUnit,InchUnit
-
-def test_givenThreeFeetAndOneYard_whenCompared_thenShouldEqual():
-    three_feet_object = Quantity(3,FeetUnit()) 
-    one_yard_object = Quantity(1,YardUnit())
-    assert three_feet_object == one_yard_object
+def test_givenTwoInchAndFiveCentiMeter_whenCompared_thenShouldEqual():
+    two_inch_object = InchUnit(2) 
+    five_cm_object = CentiMeterUnit(5)
+    assert two_inch_object == five_cm_object
 
 
-def test_givenOneFeetAndOneYard_whenCompared_thenShouldNotEqual():
-    with pytest.raises(AssertionError) as e:
-        one_feet_object = Quantity(1,FeetUnit()) 
-        one_yard_object = Quantity(1,YardUnit())
-        assert one_feet_object != one_yard_object
-
-
-def test_givenOneInchAndOneYard_whenCompared_thenShouldNotEqual():
-    with pytest.raises(AssertionError) as e:
-        one_inch_object = Quantity(1,InchUnit()) 
-        one_yard_object = Quantity(1,YardUnit())
-        assert one_inch_object != one_yard_object
-
-
-def test_givenOneYardAndThirtySixInch_whenCompared_thenShouldEqual():
-    one_yard_object = Quantity(1,YardUnit())
-    thirtySix_inch_object = Quantity(36,InchUnit()) 
-    assert one_yard_object == thirtySix_inch_object
-
-
-def test_givenThirtySixInchAndOneYard_whenCompared_thenShouldEqual():
-    thirtySix_inch_object = Quantity(36,InchUnit()) 
-    one_yard_object = Quantity(1,YardUnit())
-    assert thirtySix_inch_object == one_yard_object
-
-
-def test_givenOneYardAndThreeFeet_whenCompared_thenShouldEqual():
-    one_yard_object = Quantity(1,YardUnit())
-    three_feet_object = Quantity(3,FeetUnit()) 
-    assert one_yard_object == three_feet_object
