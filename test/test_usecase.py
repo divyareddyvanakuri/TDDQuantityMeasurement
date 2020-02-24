cf205f870b3567b0ca644c49cd893f6c177cbd86
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-24 11:46:22 +0530

    [Divya]refactor: Imported the inch and centimeter classes from quantity file and also composition class quantity from main file

--- test/test_usecase.py
+++ test/test_usecase.py
@@ -1,6 +1,14 @@
+import pytest
+import sys
+sys.path.append("/home/user/Desktop/TDDQuantityMeasurment")
+from main import Quantity
+sys.path.append("/home/user/Desktop/TDDQuantityMeasurment/src")
+from quantitymeasurment import InchUnit,CentiMeterUnit
+
+
 def test_givenTwoInchAndFiveCentiMeter_whenCompared_thenShouldEqual():
-    two_inch_object = InchUnit(2) 
-    five_cm_object = CentiMeterUnit(5)
+    two_inch_object = Quantity(2,InchUnit()) 
+    five_cm_object = Quantity(5,CentiMeterUnit())
     assert two_inch_object == five_cm_object
 
 
